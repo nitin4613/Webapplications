5c1a1dc8359910aa1c139bf2de186c22554d3b72
nitin4613 <nitin.kuriakose@gmail.com>
2021-11-02 12:15:15 -0700

    +Changes made

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import sqlite3
+from sqlite3.dbapi2 import connect
 from flask import Flask
 from flask import request
 from flask import render_template
@@ -5,10 +7,20 @@ from flask import render_template
 
 def create_app():
     app = Flask(__name__)
-    
+
     @app.route("/")
     def home():
-        return render_template('hello.html')
+        database_server = sqlite3.connect(':memory:')
+        database_server.row_factory = sqlite3.Row
+       
+        database_server.execute("CREATE TABLE IF NOT EXISTS userInfo(fname VARCHAR(20) PRIMARY KEY, mname VARCHAR(20), lname VARCHAR(20) );")
+        database_server.execute("""INSERT INTO userInfo(fname) Values("CircleCi, CoolCi, CoolerCi");""")
+        database_server.commit()
+
+        posts = database_server.execute('SELECT * FROM userInfo').fetchall()
+        #print(posts)
+        database_server.close()
+        return render_template('hello.html', posts=posts)
 
     def shutdown_server():
         func = request.environ.get('werkzeug.server.shutdown')
