f0cbb78284a54329671249390bbcb359eba65455
your_github_username <nitin@circleci.com>
2024-02-09 02:02:39 -0800

    Changes to the frontend by updating flask cli

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os
-import time
+import click
+from flask import Flask
+
 COV = None
 if os.environ.get('FLASK_COVERAGE'):
     import coverage
@@ -15,36 +17,35 @@ if os.path.exists('.env'):
             os.environ[var[0]] = var[1]
 
 from app import create_app
-from flask_script import Manager
 
 app = create_app()
-manager = Manager(app)
 
-@manager.command
-def test(coverage=False):
+@app.cli.command()
+@click.option('--coverage', is_flag=True, help='Run tests under code coverage.')
+def test(coverage):
     """Run the unit tests."""
     if coverage and not os.environ.get('FLASK_COVERAGE'):
         import sys
         os.environ['FLASK_COVERAGE'] = '1'
         os.execvp(sys.executable, [sys.executable] + sys.argv)
-    time.sleep(1)
-    import unittest
-    import xmlrunner
-    tests = unittest.TestLoader().discover('tests')
-    # run tests with unittest-xml-reporting and output to $CIRCLE_TEST_REPORTS on CircleCI or test-reports locally
-    xmlrunner.XMLTestRunner(output=os.environ.get('CIRCLE_TEST_REPORTS','test-reports')).run(tests)
-    if COV:
-        COV.stop()
-        COV.save()
-        print('Coverage Summary:')
-        COV.report()
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        covdir = os.path.join(basedir, 'tmp/coverage')
-        COV.html_report(directory=covdir)
-        print('HTML version: file://%s/index.html' % covdir)
-        COV.erase()
-
-    
+    else:
+        # Ensure this block does not run again if restarted for coverage
+        time.sleep(1)
+        import unittest
+        import xmlrunner
+        tests = unittest.TestLoader().discover('tests')
+        # run tests with unittest-xml-reporting and output to test-reports locally
+        xmlrunner.XMLTestRunner(output=os.environ.get('CIRCLE_TEST_REPORTS', 'test-reports')).run(tests)
+        if COV:
+            COV.stop()
+            COV.save()
+            print('Coverage Summary:')
+            COV.report()
+            basedir = os.path.abspath(os.path.dirname(__file__))
+            covdir = os.path.join(basedir, 'tmp/coverage')
+            COV.html_report(directory=covdir)
+            print('HTML version: file://%s/index.html' % covdir)
+            COV.erase()
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
